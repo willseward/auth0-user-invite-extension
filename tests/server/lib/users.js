50122706dfb5ed46277ef860ba593245d9576f45
Luke Bond <luke.n.bond@gmail.com>
2016-08-19 15:07:49 +0100

    Added some basic lib/user.js tests with nock

--- tests/server/lib/users.js
+++ tests/server/lib/users.js
@@ -19,22 +19,15 @@ describe('users', function () {
   });
 
   describe('getUsers', function (done) {
-    it('does something', function (done) {
-      var request = nock('https://user-invite-extension.auth0.com:443', {"encodedQueryParams":true})
+    beforeEach(function () {
+      nock.cleanAll();
+    });
+
+    it('calls the auth0 v2 api users endpoint', function (done) {
+      let request = nock('https://user-invite-extension.auth0.com:443', {"encodedQueryParams":true})
         .get('/api/v2/users')
         .query({"sort":"last_login%3A-1","q":"app_metadata.invite.status%3Apending","per_page":"100","page":"0","include_totals":"true","fields":"user_id%2Cname%2Cemail%2Capp_metadata","search_engine":"v2"})
-        .reply(200, ["1f8b08000000000000031d8ec10e82301044ff65cf60baad6d9593ff618c69edaa8d50082c2606f977176f6f2633995960e2303234aa8236775908d5c6541efc1451c13cd13841735efe74cd091a08333fd5d7fa68f6ee7870680e88d139434a69f250017521b7126ce7179d3e6ddae55edc300cd78e38a4c0019a05727967a68de405cfb202039594cb43c2dcbfa888a36c4474ded4f17ef7f5deba541f755235c51b598c9aac4658d7f5b25538c82aae3ff8b66f60d7000000"], { date: 'Wed, 17 Aug 2016 15:10:19 GMT',
-        'content-type': 'application/json; charset=utf-8',
-        'transfer-encoding': 'chunked',
-        connection: 'close',
-        'x-ratelimit-limit': '50',
-        'x-ratelimit-remaining': '48',
-        'x-ratelimit-reset': '1471446620',
-        vary: 'origin,accept-encoding',
-        'cache-control': 'no-cache',
-        'content-encoding': 'gzip',
-        'strict-transport-security': 'max-age=15724800',
-        'x-robots-tag': 'noindex, nofollow, nosnippet, noarchive' });
+        .reply(200);
 
       let options = {
         auth0: auth0,
@@ -44,9 +37,68 @@ describe('users', function () {
       };
       users.getUsers(options, function (err, result) {
         expect(request.isDone()).to.be.true;
-        expect(err).not.to.be.ok;
         return done();
       });
+    });
+
+    it('handles the case where there are no users', function (done) {
+      let request = nock('https://user-invite-extension.auth0.com:443', {"encodedQueryParams":true})
+        .get('/api/v2/users')
+        .query({"sort":"last_login%3A-1","q":"app_metadata.invite.status%3Apending","per_page":"100","page":"0","include_totals":"true","fields":"user_id%2Cname%2Cemail%2Capp_metadata","search_engine":"v2"})
+        .reply(200, { start: 0, limit: 100, length: 0, users: [], total: 0 });
+
+      let options = {
+        auth0: auth0,
+        filter: 'pending',
+        perPage: 100,
+        page: 0
+      };
+      users.getUsers(options, function (err, result) {
+        expect(err).to.not.be.ok;
+        return done();
+      });
+    });
+
+    it('returns an error object when a bad request is made', function (done) {
+      let request = nock('https://user-invite-extension.auth0.com:443', {"encodedQueryParams":true})
+        .get('/api/v2/users')
+        .query({"sort":"last_login%3A-1","q":"app_metadata.invite.status%3Apending","per_page":"100","page":"0","include_totals":"true","fields":"user_id%2Cname%2Cemail%2Capp_metadata","search_engine":"v2"})
+        .reply(400);
+
+      let options = {
+        auth0: auth0,
+        filter: 'pending',
+        perPage: 100,
+        page: 0
+      };
+      users.getUsers(options, function (err, result) {
+        expect(err).to.be.ok;
+        return done();
+      });
+    });
+
+    it('returns an error object when an error comes back from the API', function (done) {
+      let request = nock('https://user-invite-extension.auth0.com:443', {"encodedQueryParams":true})
+        .get('/api/v2/users')
+        .query({"sort":"last_login%3A-1","q":"app_metadata.invite.status%3Apending","per_page":"100","page":"0","include_totals":"true","fields":"user_id%2Cname%2Cemail%2Capp_metadata","search_engine":"v2"})
+        .reply(500);
+
+      let options = {
+        auth0: auth0,
+        filter: 'pending',
+        perPage: 100,
+        page: 0
+      };
+      users.getUsers(options, function (err, result) {
+        expect(err).to.be.ok;
+        return done();
+      });
+    });
+
+    it.skip('"pending" filter returns just pending users', function (done) {
+    });
+
+    it.skip('"accepted" filter returns just accepted users', function (done) {
     });
   });
 
